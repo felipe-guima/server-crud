08cd3947db88d5e1d6ab63c77bc87d20c173a8a5
Felipe Guimaraes <feelipe.guima@gmail.com>
2024-04-10 00:40:45 -0300

    api config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { sql } = require("./app");
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get("/getCards", async (req, res) => {
   try {
